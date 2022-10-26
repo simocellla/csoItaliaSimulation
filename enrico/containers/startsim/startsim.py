a3557426f72046b01eaaa172bcf71073a0888420
Enrico RUsso <enrico.russo@unige.it>
2022-10-26 13:22:52 +0200

    nuove mods

--- enrico/containers/startsim/startsim.py
+++ enrico/containers/startsim/startsim.py
@@ -3,12 +3,13 @@ import os
 import threading
 import time
 import json
-from flask import Flask
+from flask import Flask, request
 
-simport = os.getenv('port', default = 8813)
-simhost = os.getenv('host', default = 'localhost')
-simlabel = os.getenv('label', default = 'main')
-webport = os.getenv('port', default = 9090)
+simport = os.getenv('SIM_PORT', default = 8813)
+simhost = os.getenv('SIM_HOST', default = 'localhost')
+simlabel = os.getenv('SIM_LABEL', default = 'main')
+webport = os.getenv('SIM_API_PORT', default = 9090)
+delay = os.getenv('DELAY', default = 200)
 
 traci.init(port=simport, host=simhost, label=simlabel)
 conn = traci.getConnection(simlabel)
@@ -16,11 +17,29 @@ conn = traci.getConnection(simlabel)
 lock = threading.Lock()
 
 app = Flask(__name__)
+
 @app.route("/")
 def test():
     with lock:
         l = conn.vehicle.getIDList()
         return json.dumps(l)
+
+# secondo std rest (da vedere)
+@app.route("/bus/<bus_id>", methods = ['POST'])
+def add_bus(bus_id):
+    # request.form['name']
+    # codice per aggiungere bus nella simulazione
+    return json.dumps(request.form)
+
+@app.route("/bus/<bus_id>/<gps>", methods = ['GET'])
+def get_gps(bus_id, gps):
+    # request.form['name']
+    # codice per aggiungere bus nella simulazione
+    #ret = 
+    #ret['bus_id'] = bus_id
+    #ret['gps'] = 123
+    return bus_id + " " + gps # json.dumps(ret)
+
 
 def simulation():
     print("Starting simulation...")
@@ -31,10 +50,12 @@ def simulation():
             sim = traci.simulation.getMinExpectedNumber() > 0
             conn.simulationStep()
             step += 1
-        time.sleep(0.1)
+        if (delay - 50) > 0: time.sleep((delay - 50)/1000)
+        #time.sleep(0.1)
 
     conn.close()
 
 if __name__ == '__main__':
+    print(delay)
     t1 = threading.Thread(target=lambda: app.run(port=webport, host='0.0.0.0', debug=True, use_reloader=False)).start()
     t2 = threading.Thread(target=simulation()).start()
