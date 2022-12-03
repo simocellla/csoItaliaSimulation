8df312311863b74fbe68a0c7a2737fdfdd9ae337
simocellla <scella822@gmail.com>
2022-12-03 16:32:32 +0100

    Added 'Python attacker'

---
+++ net_working/injection_speed.py
@@ -0,0 +1,50 @@
+import requests
+import time
+import sys
+
+url_post = 'http://127.0.0.1:9393/updatebus/'
+url_clean_bus = "http://127.0.0.1:9393/busclean/"
+speed = 0
+bus_name = None
+
+def __init__():
+    global speed
+    global url_post
+    global url_clean_bus
+    global speed
+    global bus_name
+    try:
+        bus_name,speed = str(sys.argv[1]), sys.argv[2]
+        url_post = url_post+bus_name
+        url_clean_bus = url_clean_bus+bus_name
+    except IndexError:
+        print('[+] Usage: python ' + sys.argv[0] + ' <Bus ID> <desidered speed>')
+        sys.exit(0)
+    print("         Bus "+bus_name+"\n"+"Desidered speed injection:",speed)
+    print("------------------------------")
+
+
+def injection():
+    lat = 0
+    lon = 0
+    try:
+        for i in range(0,10000):
+            todo = {"lat": lat, "lon": lon, "speed": speed}
+            response = requests.post(url_post, data=todo)
+            print(f"iteration",i,": ",response)
+            time.sleep(0.1)
+    except:
+        print("[+] Connection problem!")
+        todo = {"bus": bus_name}
+        response = requests.post(url_clean_bus, data=todo)
+        print("Before exit",response.status_code)
+        sys.exit(0)
+
+
+'''
+    Main program
+'''
+if __name__ == '__main__':
+    __init__()
+    injection()
+    time.sleep(0.1)
